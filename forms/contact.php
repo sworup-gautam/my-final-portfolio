bd4c0b448dfbd3c2520a0b41211d32fa9b3263ae
Sworup Gautam <69860269+sworup-gautam@users.noreply.github.com>
2026-09-08 14:42:17 +0545

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -9,7 +9,7 @@
   // Replaced contact@example.com with real receiving email address
   $receiving_email_address = 'sworupgautam@gmail.com';
 
-  if( file_exists($php_email_form = './assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = './assets/vendor/php-email-form/validate.js' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
